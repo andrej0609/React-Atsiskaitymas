ade940bcf687248a8dc1c482fbc164b4cbfb7818
andrej0609 <andrejus.galkinas@gmail.com>
2023-02-02 14:23:56 +0200

    Pakeisti duomenys

--- atsiskaitymas/src/components/Menu/Header.jsx
+++ atsiskaitymas/src/components/Menu/Header.jsx
@@ -2,7 +2,6 @@ import UserInfo from '../User/UserInfo';
 import UserContext from "../../contexts/UserContext";
 import { useContext } from "react";
 import { Link, Outlet } from 'react-router-dom';
-import Footer from './Footer';
 import WebLogo from './WebLogo';
 
 const Header = () => {
@@ -22,7 +21,6 @@ const Header = () => {
       }
       <hr className='line' />
       <Outlet />
-      <Footer />
     </>
   );
 }
