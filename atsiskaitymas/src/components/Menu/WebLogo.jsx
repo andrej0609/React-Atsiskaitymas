e6247cb3740f83f9d2da27c868262e657160bdc2
andrej0609 <andrejus.galkinas@gmail.com>
2023-02-02 13:00:11 +0200

    pakurtas logotipas

---
+++ atsiskaitymas/src/components/Menu/WebLogo.jsx
@@ -0,0 +1,9 @@
+const WebLogo = () => {
+  return (
+    <div className="logo">
+      <img src="https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2019/08/Website-logo-1.png" alt="logo" />
+    </div>
+  );
+}
+
+export default WebLogo;
